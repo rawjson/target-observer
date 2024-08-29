562347e3d02b53ab7587277b4c9e93f0b6f712c1
Navninder <navninderbenipal@gmail.com>
2024-08-29 16:31:39 +0530

    initialize new npm package

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    build: {
+        emptyOutDir: false,
+        lib: {
+            entry: path.resolve(__dirname, 'src/main.ts'),
+            formats: ['es'],
+            name: 'useInView'
+        },
+        rollupOptions: {
+            external: ['react'],
+            output: {
+                globals: {
+                    React: 'react'
+                }
+            }
+        }
+    },
+    plugins: [react(), dts()]
+})
